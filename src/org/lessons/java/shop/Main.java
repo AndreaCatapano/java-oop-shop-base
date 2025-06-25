0cdc9869a5e9f188cf34e6ef6f47be421a8f17f3
AndreaCatapano <a.catapano96@gmail.com>
2025-06-25 17:06:43 +0200

    fix: changed float with BigDecimal

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,18 +1,20 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        Product maglietta = new Product("t-shirt", "t-shirt gialla", 100);
-        Product pantalone = new Product("pantalone", "pantalone nero", 50);
-        Product maglione = new Product("maglione", "maglione rosso", 200);
+        Product maglietta = new Product("t-shirt", "t-shirt gialla", new BigDecimal("100"));
+        Product pantalone = new Product("pantalone", "pantalone nero", new BigDecimal("50"));
+        Product maglione = new Product("maglione", "maglione rosso", new BigDecimal("200"));
 
-        System.out.println(maglietta.showPrice());
-        System.out.println(pantalone.showPrice());
-        System.out.println(maglione.showPrice());
+        System.out.println(maglietta.getPrice());
+        System.out.println(pantalone.getPrice());
+        System.out.println(maglione.getPrice());
 
-        System.out.println(maglietta.showFullPrice());
-        System.out.println(pantalone.showFullPrice());
-        System.out.println(maglione.showFullPrice());
+        System.out.println(maglietta.getFullPrice());
+        System.out.println(pantalone.getFullPrice());
+        System.out.println(maglione.getFullPrice());
 
         System.out.println(maglietta.showFullName());
         System.out.println(pantalone.showFullName());
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,29 +1,38 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Product {
     int serialCode;
     String name;
     String description;
-    float price;
-    float tax;
+    BigDecimal price;
+    BigDecimal tax;
 
-    Product(String name, String description, float price) {
+    Product(String name, String description, BigDecimal price) {
         this.serialCode = (int) (Math.random() * 900000) + 100000;
         this.name = name;
         this.description = description;
         this.price = price;
-        this.tax = 0.22f;
+        this.tax = new BigDecimal("0.22");
     }
 
-    public float showPrice() {
+    public BigDecimal getPrice() {
         return this.price;
     }
 
-    public float showFullPrice() {
-        return this.price * (1 + tax);
+    public BigDecimal getFullPrice() {
+        if (price != null && tax != null) {
+            return price.add(price.multiply(tax).setScale(2, RoundingMode.DOWN));
+        }
+        return null;
     }
 
     public String showFullName() {
-        return serialCode + "-" + name;
+        if (name != null) {
+            return serialCode + "-" + name;
+        }
+        return null;
     }
 }
