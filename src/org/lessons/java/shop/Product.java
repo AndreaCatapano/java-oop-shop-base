2e3a831e0f7d913e6b4269c09a73b9e89beb86c8
AndreaCatapano <a.catapano96@gmail.com>
2025-06-25 16:48:33 +0200

    feat: added methods in Product Class

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -7,11 +7,23 @@ public class Product {
     float price;
     float tax;
 
-    Product(int serialCode, String name, String description, float price) {
-        this.serialCode = serialCode;
+    Product(String name, String description, float price) {
+        this.serialCode = (int) (Math.random() * 900000) + 100000;
         this.name = name;
         this.description = description;
         this.price = price;
-        this.tax = 22.00f;
+        this.tax = 0.22f;
+    }
+
+    public float showPrice() {
+        return this.price;
+    }
+
+    public float showFullPrice() {
+        return this.price * (1 + tax);
+    }
+
+    public String showFullName() {
+        return serialCode + "-" + name;
     }
 }
