a62a5c17e78eb767fddbc1178bc4e86998e83144
AndreaCatapano <a.catapano96@gmail.com>
2025-06-25 16:38:03 +0200

    feat: added a constructor in Product Class

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,5 +1,17 @@
 package org.lessons.java.shop;
 
 public class Product {
+    int serialCode;
+    String name;
+    String description;
+    float price;
+    float tax;
 
+    Product(int serialCode, String name, String description, float price) {
+        this.serialCode = serialCode;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.tax = 22.00f;
+    }
 }
