c703af179a7a7298980157d212aea78b638d39d3
AndreaCatapano <a.catapano96@gmail.com>
2025-06-26 17:22:54 +0200

    feat: update with incapsulamento

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -6,19 +6,26 @@ public class Main {
     public static void main(String[] args) {
         Product maglietta = new Product("t-shirt", "t-shirt gialla", new BigDecimal("100"));
         Product pantalone = new Product("pantalone", "pantalone nero", new BigDecimal("50"));
-        Product maglione = new Product("maglione", "maglione rosso", new BigDecimal("200"));
+        Product pane = new Product("pane", new BigDecimal("1"), new BigDecimal("0.04"));
 
         System.out.println(maglietta.getPrice());
         System.out.println(pantalone.getPrice());
-        System.out.println(maglione.getPrice());
+        System.out.println(pane.getPrice());
 
         System.out.println(maglietta.getFullPrice());
         System.out.println(pantalone.getFullPrice());
-        System.out.println(maglione.getFullPrice());
 
-        System.out.println(maglietta.showFullName());
-        System.out.println(pantalone.showFullName());
-        System.out.println(maglione.showFullName());
+        System.out.println(pane.getFullName());
+        System.out.println(pane.getFullPrice());
+
+        pane.setName("Pane Integrale");
+        pane.setPrice(new BigDecimal("1.5"));
+
+        System.out.println(pane.getFullPrice());
+
+        System.out.println(maglietta.getFullName());
+        System.out.println(pantalone.getFullName());
+        System.out.println(pane.getFullName());
 
     }
 }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -4,13 +4,13 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Product {
-    int serialCode;
-    String name;
-    String description;
-    BigDecimal price;
-    BigDecimal tax;
+    private int serialCode;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private BigDecimal tax;
 
-    Product(String name, String description, BigDecimal price) {
+    public Product(String name, String description, BigDecimal price) {
         this.serialCode = (int) (Math.random() * 900000) + 100000;
         this.name = name;
         this.description = description;
@@ -18,18 +18,62 @@ public class Product {
         this.tax = new BigDecimal("0.22");
     }
 
+    public Product(String name, BigDecimal price, BigDecimal tax) {
+        this.serialCode = (int) (Math.random() * 900000) + 100000;
+        this.name = name;
+        this.description = name;
+        this.price = price;
+        this.tax = tax;
+    }
+
+    // GETTER AND SETTER
+
+    public int getSerialCode() {
+        return this.serialCode;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public BigDecimal getPrice() {
         return this.price;
     }
 
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getTax() {
+        return this.tax;
+    }
+
+    public void setTax(BigDecimal tax) {
+        this.tax = tax;
+    }
+
+    // UTILITY
+
     public BigDecimal getFullPrice() {
-        if (price != null) {
+        if (price != null && tax != null) {
             return price.add(price.multiply(tax).setScale(2, RoundingMode.DOWN));
         }
         return null;
     }
 
-    public String showFullName() {
+    public String getFullName() {
         if (name != null) {
             return serialCode + "-" + name;
         }
